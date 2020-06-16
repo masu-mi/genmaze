47b47dd55bc56db880dc641828b33c531e7ccae2
Kanai Masumi <masumi.net@gmail.com>
2020-06-16 22:05:28 +0900

    Make client's URL switchable

--- svgmaze/src/client.py
+++ svgmaze/src/client.py
@@ -1,12 +1,16 @@
 # vim: fileencoding=utf-8
 
+import sys
 import grpc
 from gen.pb import maze_pb2,maze_pb2_grpc
-import base64
 
+url = 'localhost:50051'
+if len(sys.argv) == 2:
+    url = sys.argv[1]
 
 def main():
-    with grpc.insecure_channel('localhost:50051') as channel:
+    print("URL:{}".format(url))
+    with grpc.insecure_channel(url) as channel:
         stub = maze_pb2_grpc.MazeStub(channel)
         response = stub.Gen(maze_pb2.GenRequest(x=25, y=25))
         print('[GENERATED]\n'+response.field)
